9da8acf564164c73e165f9d5a1dbdad618a3ddeb
ramprasadsuthi <ramprasadsuthi@gmail.com>
2022-12-21 20:00:53 +0530

    Approved

--- src/sampleScripts/Test1.java
+++ src/sampleScripts/Test1.java
@@ -5,8 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello I am adding this line from Eclipse..");
-		System.out.println("And This line is added from GITHUB...");
+		System.out.println("Hello I am adding lines from Eclipse..");
+
 	}
 
 }
